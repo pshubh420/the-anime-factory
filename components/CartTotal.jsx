477b11ff1518739ce549b6d775719655e7026f75
shubh patel <pshubh420@gmail.com>
2025-04-13 16:46:29 +0530

    modified:   components/CartTotal.jsx

--- components/CartTotal.jsx
+++ components/CartTotal.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
 
 const CartTotal = () => {
 
